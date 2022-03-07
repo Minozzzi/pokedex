a7fd20da56ef93e05ebd7f3f9233e00ffb493c05
Guilherme Minozzi <minozzi60@gmail.com>
2022-03-07 19:50:20 -0300

    feat: ensure remote pokemon calls HttpGetClient with correct URL

---
+++ src/data/usecases/pokemon/remote-pokemon.ts
@@ -0,0 +1,12 @@
+import { HttpGetClient } from '../../protocols/http/http-get-client'
+
+export class RemotePokemon {
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient
+  ) {}
+
+  async findAll(): Promise<void> {
+    await this.httpGetClient.get(this.url)
+  }
+}
