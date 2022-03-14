d38c6bde3ba99eec9e5d31d4501dea0c8da5edb6
Guilherme Minozzi <minozzi60@gmail.com>
2022-03-14 19:26:56 -0300

    refactor: change models of pokemon and pokemons

--- src/domain/models/pokemon-model.ts
+++ src/domain/models/pokemon-model.ts
@@ -1,9 +1,39 @@
-export type IPokemonProps = {
+type Abilities = {
+  ability: {
+    name: string
+  }
+  slot: number
+}[]
+
+type Types = {
+  slot: number
+  type: {
+    name: string
+  }
+}[]
+
+type Stats = {
+  base_stat: number
+  effort: number
+  stat: {
+    name: string
+  }
+}[]
+
+export type IPokemonsProps = {
   count: number
   next: string | null
   previous: string | null
   result: {
     name: string
     url: string
-  }[]
+  }
+}
+
+export type IPokemonProps = {
+  height: number
+  weight: number
+  abilities: Abilities
+  types: Types
+  stat: Stats
 }
