271217f608dd875171654aea5d5b8898aebf3df8
Guilherme Minozzi <minozzi60@gmail.com>
2022-04-11 20:36:09 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
-export class AxiosHttpClient {
-  async get(url: string): Promise<void> {
-    await axios.get(url)
+import { HttpGetClient } from '@/data/protocols/http/http-get-client'
+import { HttpResponse } from '@/data/protocols/http/http-response'
+
+export class AxiosHttpClient implements HttpGetClient<unknown> {
+  async get(url: string): Promise<HttpResponse<unknown>> {
+    const httpResponse = await axios.get(url)
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data
+    }
   }
 }
